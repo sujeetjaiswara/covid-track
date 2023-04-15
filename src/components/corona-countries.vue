213abd875175a32d4f3ece4bcbeb6200cf54e422
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2023-04-15 17:01:11 +0530

    Completed - row click event implimention

--- src/components/corona-countries.vue
+++ src/components/corona-countries.vue
@@ -8,16 +8,17 @@
       </v-col>
       <v-col lg="4" md="4" sm="4" cols="12">
         <v-text-field v-model="search" label="Search country" append-inner-icon="mdi-magnify" single-line hide-details
-        density="compact" variant="outlined" class="w-50 float-right" :height="20" color="#000"></v-text-field>
+          density="compact" variant="outlined" class="w-50 float-right" :height="20" color="#000"></v-text-field>
       </v-col>
     </v-row>
 
-    <v-data-table :headers="headers" :items="cloneCountries" :items-per-page="10" class="elevation-0 mt-5 border rounded pb-3"
-      :loading="isLoading" loading-text="Loading.." :search="search" color="primary">
+    <v-data-table :headers="headers" :items="cloneCountries" :items-per-page="10"
+      class="elevation-0 mt-5 border rounded pb-3" :loading="isLoading" loading-text="Loading.." :search="search"
+      color="primary" item-key="country" :single-select="true" @click:row="getStatusByCountry">
 
       <!-- Country -->
       <template v-slot:item.country="{ item }">
-        <div @click="getStatusByCountry(item.raw.country)" class="d-flex align-center country-link">
+        <div class="d-flex align-center country-link">
           <v-img class="mr-2 float-left" :lazy-src="item.raw.countryInfo.flag" :src="item.raw.countryInfo.flag"
             max-width="28">
             <template v-slot:placeholder>
@@ -112,7 +113,11 @@ onMounted(() => {
   cloneCountries.value = [...props.countries];
 });
 
-function getStatusByCountry(countryName: string) {
+function getStatusByCountry(event: Event, row: any) {
+  event.stopPropagation();
+
+  const _row = JSON.parse(JSON.stringify(row));
+  const countryName = _row.item.value.country;
   emit("getStatusByCountry", countryName);
 }
 </script>
