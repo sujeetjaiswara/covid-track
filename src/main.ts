b1d709dc0509e9f67d2f854edfb411175ab6d88c
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2023-04-15 18:25:07 +0530

    Fixed - plugin path

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-import vuetifyPlugins from './plugins/vuetify.js';
+import vuetifyPlugins from '@/plugins/vuetify.js';
 
 import './assets/main.css'
 
