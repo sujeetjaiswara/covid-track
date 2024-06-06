f5ed546869e9b85f39f6b240e8bd0e19cfec288a
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2024-06-06 18:08:46 +0530

    Created - app menu component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { useTheme } from 'vuetify'
-
-const theme = useTheme()
-
-// Set dark mode if user system prefers scheme
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  theme.global.name.value = 'dark'
-}
-
-const onToggleTheme = () => {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-}
+import CvMenu from '@/components/cv-menu.vue'
 </script>
 
 <template>
   <v-layout class="px-5 pt-3">
-    <!-- Menu -->
-    <v-app-bar color="primary" elevation="0">
-      <v-app-bar-title><span class="me-2 fs-2">🦠</span>Covid Track</v-app-bar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click="onToggleTheme">
-        <v-icon v-if="theme.global.current.value.dark">mdi-weather-night</v-icon>
-        <v-icon v-else color="amber">mdi-white-balance-sunny</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <!-- Content -->
+    <CvMenu />
     <v-main class="main-content">
       <RouterView />
     </v-main>
---
+++ src/components/cv-menu.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+onMounted(() => {
+  // Set dark mode if user system prefers scheme
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    theme.global.name.value = 'dark'
+  }
+})
+
+const onToggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <v-app-bar color="primary" elevation="0">
+    <v-app-bar-title><span class="me-2 fs-2">🦠</span>Covid Track</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon @click="onToggleTheme">
+      <v-icon v-if="theme.global.current.value.dark">mdi-weather-night</v-icon>
+      <v-icon v-else color="amber">mdi-white-balance-sunny</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
